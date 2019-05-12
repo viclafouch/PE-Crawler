6abf0da895e4d1695f927b0d7dc9a6395d791612
viclafouch <victor.dlf@outlook.fr>
2019-05-12 12:18:09 +0200

    add route /threads

---
+++ src/routes/api/threads.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import models from '../../models'
+import { Op } from 'sequelize'
+import { languages } from '../../config'
+
+const router = express.Router()
+
+router.post('/threads', async (req, res) => {
+  const lang = req.body.lang && languages.includes(req.body.lang) ? req.body.lang : 'en'
+  const where = { lang }
+  const products = await models.Product.findAll({
+    where: {
+      id: {
+        [Op.in]: Array.isArray(req.body.productsId) ? req.body.productsId : [req.body.productsId]
+      }
+    }
+  })
+
+  const result = []
+
+  for (const product of products) {
+    where.ProductId = product.id
+    const threads = await models.Thread.findAll({
+      where,
+      attributes: ['title', 'url', 'uuid', 'description', 'lang'],
+      order: [['id', 'ASC']]
+    })
+    result.push({
+      product,
+      threads,
+      lang
+    })
+  }
+
+  return res.status(200).json({ result })
+})
+
+export default router
