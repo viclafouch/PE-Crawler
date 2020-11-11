904d7f4240d29f8653d8067d9537432d30871762
viclafouch <victor.dlf@outlook.fr>
2020-11-11 17:04:16 +0100

    chore: add constants ANSWERS_PER_PAGE

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -2,4 +2,6 @@ const BASE_URL = new URL('https://support.google.com')
 
 const IS_DEV = process.env.NODE_ENV !== 'production'
 
-export { BASE_URL, IS_DEV }
+const ANSWERS_PER_PAGE = 10
+
+export { BASE_URL, IS_DEV, ANSWERS_PER_PAGE }
